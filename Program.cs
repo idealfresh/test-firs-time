ef24b8ea8a67245ac7c49fc3889499ebb67e8388
idealfresh <idealfresh@abv.bg>
2017-01-17 19:14:27 +0200

    new test

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proba_chetna_ne_chetna
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var sumEven = 0;
+            var sumOdd = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                var number = int.Parse(Console.ReadLine());
+
+                if (i%2==0)
+                {
+                    sumEven += number;
+                }
+                else 
+                {
+                    sumOdd += number;
+                }
+            }
+            if (sumEven==sumOdd)
+            {
+                Console.WriteLine("Yes Sum = {0}", sumEven);
+            }
+            else
+            {
+                Console.WriteLine("No Diff = {0}", Math.Abs(sumEven-sumOdd));
+            }
+        
+        }
+    }
+}
